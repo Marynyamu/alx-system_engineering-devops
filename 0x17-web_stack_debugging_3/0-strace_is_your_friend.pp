dbb39681fb43827cf02606cdcaf43082472b4a38
Mary Nyamu <125545040+Marynyamu@users.noreply.github.com>
2023-11-16 17:54:00 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
  Apache 500 error
-
+#First line contains a comment explaining what your Puppet script is about
 exec { 'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
